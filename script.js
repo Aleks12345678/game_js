c41508401e206ec1b16f828d3dd46ab4b1fb8d70
Aleks12345678 <56092640+Aleks12345678@users.noreply.github.com>
2020-08-30 23:50:05 +0500

    исправлено бесконечное количество попыток

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 'use strict';
-function restart (attempts = 10 ) {
 
-    let randomNumber = Math.floor(Math.random() * 100) + 1;// рандомное число
+let randomNumber = Math.floor(Math.random() * 100) + 1;// рандомное число
+function restart (rand, attempts = 10 ) {
 
-    // let attempts = 10; //кол-во попыток
-
-    let userEntered = prompt('Введите число. У Вас 10 попыток');
-   
-    function guessing () {
-        attempts--;
+let userEntered = prompt('Введите число. У Вас '+attempts+' попыток');
         
         if (attempts === 0) {
             alert('Попытки закончились'); //закончились попытки
-            
+            return false;
         }
         
         if (isNaN(userEntered)) {
@@ -25,24 +20,18 @@ function restart (attempts = 10 ) {
 
         if (userEntered == randomNumber) {
             alert('Верно, Вы угадали загаданное число!');
-            
+            return false;
     
         } else if (userEntered < randomNumber) {
-            alert('Загаданное число больше! Осталось попыток: ' + attempts);
-            userEntered = +prompt('Введите число');
-            
-    
+            alert('Загаданное число больше!');
+                
         } else if (userEntered > randomNumber) {
-            alert('Загаданное число меньше! Осталось попыток: ' + attempts);
-            userEntered = +prompt('Введите число');           
+            alert('Загаданное число меньше!');
         }
+        attempts--;
         
-    
+        restart(rand, attempts);
+    }
+    restart(randomNumber, 10);
         
 
-        return guessing();
-    }
-    guessing();
-        
-}
-restart();
